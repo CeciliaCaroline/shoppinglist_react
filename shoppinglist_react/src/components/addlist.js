e230f53faad6d559e03db5506533840ce3fea96c
CeciliaCaroline <nalubegac58@gmail.com>
2017-09-27 00:29:56 +0300

    Added modal for creating a new list

--- shoppinglist_react/src/components/addlist.js
+++ shoppinglist_react/src/components/addlist.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Modal from 'react-modal';
 
 
 class AddList extends Component {
@@ -9,6 +10,12 @@ class AddList extends Component {
             title: '',
             description: ''
         };
+        this.state = {
+            modalIsOpen: false
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
 
     }
 
@@ -26,42 +33,73 @@ class AddList extends Component {
     };
 
 
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
+
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleSubmit.bind(this)} className=" items col-md-offset-4 col-md-4 ">
-                    <h2 className="text-center">Add New List</h2>
 
-                    <div className="form-group">
-                        <label htmlFor="title">Title</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="title"
-                            required
-                            ref='title'
-                            onChange={event => (this.setState({title: event.target.value}))}
-                        />
-                    </div>
+            <div className="container">
+                <button className=" items btn btn-info" onClick={this.openModal}>Create
+                    New List
+                </button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                    contentLabel="Example Modal"
+                    bsSize="small"
+                >
 
-                    <div className="form-group">
-                        <label htmlFor="description">Description</label>
-                        <textarea
-                            type="text"
-                            className="form-control"
-                            id="description"
-                            required
-                            ref='description'
-                            onChange={event => (this.setState({description: event.target.value}))}
-                        />
-                    </div>
+                    <div className="modal-body">
+                        <form onSubmit={this.handleSubmit} id="newList" className="col-md-offset-4 col-md-4 ">
+                            <h2 className="text-center">Add New List</h2>
 
-                    <div className="btn-group">
-                        <button type="submit" className="btn btn-success">Save
-                        </button>
+                            <div className="form-group">
+                                <label htmlFor="title">Title</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="title"
+                                    required
+                                    ref='title'
+                                    onChange={event => (this.setState({title: event.target.value}))}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="description">Description</label>
+                                <textarea
+                                    type="text"
+                                    className="form-control"
+                                    id="description"
+                                    required
+                                    ref='description'
+                                    onChange={event => (this.setState({description: event.target.value}))}
+                                />
+                            </div>
+
+                            <div>
+                                <button type="submit" className="btn btn-success">Save
+                                </button>
+                                <button type="button" className="btn btn-success"
+                                        onClick={this.closeModal}>X
+                                </button>
+                            </div>
+
+                        </form>
                     </div>
-                </form>
+                </Modal>
+
+
             </div>
+
 
 
         );
