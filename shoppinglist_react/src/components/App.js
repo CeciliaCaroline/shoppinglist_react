2c67500102dbb103efa236ef7882ee79ef247ec3
CeciliaCaroline <nalubegac58@gmail.com>
2017-09-29 15:39:38 +0300

    Refactored the app and index files

--- shoppinglist_react/src/components/App.js
+++ shoppinglist_react/src/components/App.js
@@ -3,15 +3,12 @@ import Register from './register';
 import Login from "./login";
 import Home from "./home";
 import ShoppingList from "./shoppinglist";
-import PropTypes from 'prop-types';
+import ResetPassword from "./reset_password";
+import Logout from "./logout";
 import {BrowserRouter, Route} from 'react-router-dom';
 
 
-let Lists = [{
-    title: "Cecilia",
-    description: "qwertyuiop",
-    id: 1
-}];
+
 
 class App extends Component {
     constructor(props) {
@@ -27,24 +24,18 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <div className="App">
-                    <Route exact path="/" component={Register}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/home" render={() => <Home initialLists={Lists}/>}/>
-                    <Route path="/shoppinglist" render={() => <ShoppingList initialLists={Lists}/>}/>
+                    <Route exact path="/auth/register" component={Register}/>
+                    <Route path="/auth/login" component={Login}/>
+                    <Route path="/auth/reset-password" component={ResetPassword}/>
+                    <Route path="/auth/logout" component={Logout}/>
+                    <Route path="/home" render={() => <Home />}/>
+                    <Route path="/shoppinglist" render={() => <ShoppingList />}/>
                 </div>
             </BrowserRouter>
         );
     }
 }
 
-App.propTypes = {
-    initialLists: PropTypes.arrayOf(PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired,
-            id: PropTypes.number.isRequired
-        }
-    )).isRequired,
 
-};
 
 export default App;
--- shoppinglist_react/src/index.js
+++ shoppinglist_react/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-let Lists = [];
 
-ReactDOM.render(<App initialLists={Lists}/>, document.getElementById('root'));
+ReactDOM.render(<App  />, document.getElementById('root'));
 
