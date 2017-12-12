7910de09167c42c7f7107cac29fe05e62b727eb0
ceciliacaroline <nalubegac58@gmail.com>
2017-12-12 20:11:03 +0300

    Create base component base component class

---
+++ shoppinglist_react/src/components/base.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+
+class BaseComponent extends Component {
+
+
+    pushNavigation = (event) => {
+        event.preventDefault();
+        this.props.history.push(event.target.getAttribute("href"));
+    }
+
+}
+
+
+export default BaseComponent
+
