782058c27a5e306e00852ce12a4642c8967b6b6f
ceciliacaroline <nalubegac58@gmail.com>
2017-12-13 19:08:05 +0300

    Remove unnecessary spaces

--- shoppinglist_react/src/components/notfound.js
+++ shoppinglist_react/src/components/notfound.js
@@ -12,14 +12,12 @@ class NotFound extends Component {
                     <div className="row">
                         <div className="  col  ">
                             <div className="card  items d-flex align-content-end text-center">
-
                                 <section className='mt-5 container'>
                                     <h3 className='text-center'>404 page not found</h3>
                                     <p className='text-center'>We are sorry but the page you are looking for does not
                                         exist.</p>
                                 </section>
                             </div>
-
                         </div>
                     </div>
                 </div>
