72acebb1b16a11a0b7c7d434df01365ab62c5315
ceciliacaroline <nalubegac58@gmail.com>
2017-12-14 11:05:54 +0300

    Added baseURL variable to base component

--- shoppinglist_react/src/components/base.js
+++ shoppinglist_react/src/components/base.js
@@ -1,12 +1,32 @@
 import {Component} from 'react';
 
 
+
+
 class BaseComponent extends Component {
+    baseURL = "http://127.0.0.1:5000";
 
     pushNavigation = (event) => {
         event.preventDefault();
         this.props.history.push(event.target.getAttribute("href"));
+    };
+
+    contentHeader = () => {
+        return {headers: {"Content-Type": "application/json"}}
+    };
+
+    authHeader = () => {
+        let contentHeader = this.contentHeader();
+        contentHeader["headers"]["authorization"] = "Bearer " + localStorage.getItem('token');
+        return contentHeader
+    };
+
+    componentDidMount() {
+        this.setState({notificationSystem: this.refs.notificationSystem});
     }
+
+
+
 }
 
 export default BaseComponent
